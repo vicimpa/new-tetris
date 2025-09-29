dc16ae14fb9c2bfad9000f5815e7dde33e0b7a78
PromiSe#### <vicimpa@ya.ru>
2025-09-29 18:28:03 +0300

    Fix timer

--- src/utils/looper.ts
+++ src/utils/looper.ts
@@ -6,7 +6,9 @@ export function looper(fn: FrameLoop) {
 
   function _fn(now = performance.now()) {
     raf = requestAnimationFrame(_fn);
-    fn(now - time, time = now);
+    var delta = now - time;
+    if (delta < 100)
+      fn(delta, time = now);
   }
 
   return () => {
