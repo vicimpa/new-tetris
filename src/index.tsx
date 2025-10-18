aaaad99bdc15b8d554690b1320b97464a1be757d
PromiSe#### <vicimpa@ya.ru>
2025-10-19 00:39:12 +0300

    Fix scale for firefox

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,13 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import { find } from "&utils/dom";
 
-createRoot(find('#app'))
+const { min } = Math;
+const app = find('#app') as HTMLDivElement;
+(onresize = resize, resize());
+
+function resize() {
+  app.style.transform = `scale(${min(innerWidth, innerHeight) / 650})`;
+}
+
+createRoot(app)
   .render(<App />);
